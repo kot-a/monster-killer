91920ad0cac1ff5df96dc360d3b25e83eae4e46a
kot_a <anita.dabrowska07@gmail.com>
2020-09-17 17:12:57 +0200

    Added Battle Log and minor CSS styling.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const attackButton = document.getElementById('attack');
 const specialAttackButton = document.getElementById('special-attack');
 const healButton = document.getElementById('heal');
 
+const battleLog = document.getElementById('battle-log');
+let displayedBattleLog = [];
+let enteredInfo;
+
 const ATTACK_VALUE = 10;
 const SPECIAL_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 24;
@@ -15,6 +19,8 @@ const MODE_SPECIAL_ATTACK = 'SPECIAL_ATTACK';
 const LOG_PLAYER_ATTACK = 'ATTACK';
 const LOG_PLAYER_SPECIAL_ATTACK = 'SPECIAL_ATTACK';
 const LOG_PLAYER_HEAL = 'HEAL';
+const LOG_MONSTER_ATTACK = 'MONSTER ATTACK';
+const GAME_OVER = 'GAME OVER';
 
 
 //----------------------------------------------------------------------------------
@@ -52,41 +58,46 @@ function endRound() {
     } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
         console.log("Draw!");
     }
+    writeToLog(LOG_MONSTER_ATTACK, Player_DamageTaken);
 }
 
 
 //----------------------------------------------------------------------------------
 //Players and Monsters Moves
 function dealMonsterDamage(damage) {
-    const dealtDamage = Math.random() * damage;
+    const dealtDamage = (Math.random() * damage).toFixed(1);
     monsterHealthBar.value = parseInt(monsterHealthBar.value) - dealtDamage;
     return dealtDamage;
 }
 
 function dealPlayerDamage(damage) {
-    const dealtDamage = Math.random() * damage;
+    const dealtDamage = (Math.random() * damage).toFixed(1);
     playerHealthBar.value = parseInt(playerHealthBar.value) - dealtDamage;
     return dealtDamage;
 }
 
 function attackMonster(mode) {
     let maxDamage;
-    if (mode === 'ATTACK') {
+    let logEvent;
+    if (mode === MODE_ATTACK) {
         maxDamage = ATTACK_VALUE;
-    } else if (mode === 'SPECIAL_ATTACK') {
+        logEvent = LOG_PLAYER_ATTACK;
+    } else if (mode === MODE_SPECIAL_ATTACK) {
         maxDamage = SPECIAL_ATTACK_VALUE;
+        logEvent = LOG_PLAYER_SPECIAL_ATTACK;
     }
     const Monster_DamageTaken = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= Monster_DamageTaken;
+    writeToLog(logEvent, Monster_DamageTaken);
     endRound();
 }
 
 function attackHandler() {
-    attackMonster('ATTACK');
+    attackMonster(MODE_ATTACK);
 }
 
 function specialAttackHandler() {
-    attackMonster('SPECIAL_ATTACK');
+    attackMonster(MODE_SPECIAL_ATTACK);
 }
 
 function increasePlayerHealth(healValue) {
@@ -95,16 +106,48 @@ function increasePlayerHealth(healValue) {
 
 function healPlayer() {
     let heal;
+    let logEntry;
     if (currentPlayerHealth >= championHealth - HEAL_VALUE) {
         heal = championHealth - currentPlayerHealth;
-        console.log(`Restored only ${Number((heal).toFixed(1))} HP - you can't healt more than your champion max health.`)
+        logEntry =`<p> >>> Restored only ${(Number(heal)).toFixed(1)} HP - you can't healt more than your champion max health.</p>`;
+        displayedBattleLog.push(logEntry);
+        showOnWeb(displayedBattleLog);
     } else {
         heal = HEAL_VALUE;
+        logEntry = `<p> >>> Restored ${(Number(heal)).toFixed(1)} HP.</p>`;
+        displayedBattleLog.push(logEntry);
+        showOnWeb(displayedBattleLog);
     }
+
     increasePlayerHealth(HEAL_VALUE);
     currentPlayerHealth += HEAL_VALUE;
     endRound();
 }
+
+//----------------------------------------------------------------------------------
+//Working on Battle Log
+
+battleLog.scrollTop = battleLog.scrollHeight;
+
+function writeToLog(event, value) {
+    let logEntry;
+    if (event === LOG_PLAYER_ATTACK){
+        logEntry = `<p> >>> You used normal attack and dealt to monster ${value} damage. </p>`
+        displayedBattleLog.push(logEntry);
+    } else if (event === LOG_PLAYER_SPECIAL_ATTACK) {
+        logEntry = `<p> >>> Special attack! You dealt to monster ${value} damag! </p>`
+        displayedBattleLog.push(logEntry);
+    } else if (event === LOG_MONSTER_ATTACK) {
+        logEntry = ` <p> >>> Monster attacked you and dealt ${value} damage to your champion. </p>`
+        displayedBattleLog.push(logEntry);
+    }
+    showOnWeb(displayedBattleLog);
+}
+
+function showOnWeb(msg){
+    battleLog.innerHTML = msg;
+}
+
 
 
 attackButton.addEventListener('click', attackHandler);
